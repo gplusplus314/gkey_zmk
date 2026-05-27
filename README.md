bfd4121019cb3083551ddef4672b148ef0edc779
Gerry Hernandez <gerry@gerryhernandez.com>
2026-05-27 11:28:06 -0700

    Switch to a Nix Flake based build

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ At this time, I don't really intend on other people using my bespoke keyboard,
 so I don't have much of a README. But here are some notes for my future self
 before I forget how to use this repo again (because it has happened):
 
-First, set up [gdot dotfiles](https://github.com/gplusplus314/gdot). Then:
+You need [Nix](https://nixos.org/download/) with flakes enabled. Then:
 
 ```sh
-gclone https://github.com/gplusplus314/gkey_zmk.git
-cd ~/src/github.com/gplusplus314/gkey_zmk/
+git clone https://github.com/gplusplus314/gkey_zmk.git
 cd gkey_zmk
-./bin/build.sh
+nix run .#flash -- left    # build + flash left half
+nix run .#flash -- right   # right half
+nix run .#flash            # both halves, in sequence
+nix run .#flash-reset      # clear ZMK settings (prompts to confirm)
+nix run .#update           # bump the pinned ZMK / Zephyr / HAL versions
 ```
 
-It will automagically set up the [Zephyr
-SDK](https://docs.zephyrproject.org/latest/develop/toolchains/zephyr_sdk.html)
-and the [ZMK Firmwary Repository](https://github.com/zmkfirmware/zmk) along with
-a local, native toolchain. By default, with no arguments, `./bin/build.sh`
-builds and flashes the left-side split of my `vibraphone` keyboard.
+Flashing is Linux-only (zmk-nix uses udisks). On macOS, do a `nix build
+.#firmware` and copy the `.uf2` file to the bootloader by hand.
---
+++ flake.nix
@@ -0,0 +1,94 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, zmk-nix }: let
+    forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+
+    src = nixpkgs.lib.sourceFilesBySuffices self [
+      ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi"
+      ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig"
+    ];
+
+    zephyrDepsHash = "sha256-7IDDbW+FtQPDJdkpy3ty5GtK+9dcAuYO8WJ5SSd4sIc=";
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      zmkLib = zmk-nix.legacyPackages.${system};
+
+      # Shared west-update FOD so firmware + firmware-reset reuse a single
+      # fetch. Without this they'd produce two store paths with identical
+      # content under different names.
+      westDeps = zmkLib.fetchZephyrDeps {
+        name = "gkey_vibraphone-west-deps";
+        inherit src;
+        westRoot = "config";
+        hash = zephyrDepsHash;
+      };
+    in rec {
+      default = firmware;
+
+      firmware = zmkLib.buildSplitKeyboard {
+        name = "gkey_vibraphone";
+        inherit src westDeps zephyrDepsHash;
+
+        board = "nice_nano";
+        shield = "gkey_vibraphone_%PART%";
+
+        meta = {
+          description = "gkey_vibraphone ZMK firmware";
+          license = nixpkgs.lib.licenses.mit;
+          platforms = nixpkgs.lib.platforms.all;
+        };
+      };
+
+      firmware-reset = zmkLib.buildKeyboard {
+        name = "gkey_vibraphone-settings-reset";
+        inherit src westDeps zephyrDepsHash;
+
+        board = "nice_nano";
+        shield = "settings_reset";
+
+        meta = {
+          description = "ZMK settings_reset firmware for nice_nano";
+          license = nixpkgs.lib.licenses.mit;
+          platforms = nixpkgs.lib.platforms.all;
+        };
+      };
+
+      flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+
+      # Settings reset is destructive (wipes BT pairings etc). Wrap the
+      # underlying flasher in a y/N prompt so a stray `nix run` can't trigger
+      # it accidentally.
+      flash-reset = pkgs.writeShellApplication {
+        name = "flash-reset";
+        text = let
+          inner = zmk-nix.packages.${system}.flash.override {
+            firmware = firmware-reset;
+          };
+        in ''
+          echo "This will clear ZMK settings (BT pairings, etc) on the side you plug in next."
+          read -r -p "Continue? (y/N): " ans
+          case "$ans" in
+            y|Y) ;;
+            *) echo "Canceled."; exit 1 ;;
+          esac
+          exec ${inner}/bin/zmk-uf2-flash "$@"
+        '';
+      };
+
+      update = zmk-nix.packages.${system}.update;
+    });
+
+    devShells = forAllSystems (system: {
+      default = zmk-nix.devShells.${system}.default;
+    });
+  };
+}
